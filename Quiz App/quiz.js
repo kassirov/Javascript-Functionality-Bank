d85e9610878225223b207710046f1ddc33c16011
Sam Kassirov <98607159+kassirov@users.noreply.github.com>
2022-06-29 14:48:31 -0500

    questions comit

---
+++ Quiz App/quiz.js
@@ -0,0 +1,84 @@
+const quizData = [
+  {
+    question: "What country has the longest coastline in the world?",
+    a: "Russia",
+    b: "Canada",
+    c: "Mexico",
+    d: "China",
+    answer: "b"
+  },
+  {
+    question: "What is the capital of Malta?",
+    a: "Victoria",
+    b: "Birgu",
+    c: "Valleta",
+    d: "Sliema",
+    answer: "c"
+  },
+  {
+    question:
+      "What country is the newest in the world to be recognised by the UN?",
+    a: "South Sudan",
+    b: "South Africa",
+    c: "Madagascar",
+    d: "Jamaica",
+    answer: "a"
+  },
+  {
+    question: "In which UK city would you find the river Clyde?",
+    a: "London",
+    b: "Glasgow",
+    c: "Liverpool",
+    d: "Colchester",
+    answer: "b"
+  },
+  {
+    question:
+      "If you travelled to the city of Volgograd, which country would be in?",
+    a: "Uzbekistan",
+    b: "Ukraine",
+    c: "Poland",
+    d: "Russia",
+    answer: "d"
+  },
+  {
+    question: "What is the capital of Canada?",
+    a: "Toronto",
+    b: "Montreal",
+    c: "Vancouver",
+    d: "Ottawa",
+    answer: "d"
+  },
+  {
+    question: "What is the largest desert in the world?",
+    a: "Sahara Desert",
+    b: "Antarctic Desert",
+    c: "Great Australian",
+    d: "Arctic Desert	",
+    answer: "b"
+  },
+  {
+    question: "How many boroughs are there in New York City?",
+    a: "13",
+    b: "9",
+    c: "5",
+    d: "3",
+    answer: "c"
+  },
+  {
+    question: "What is the capital of Peru?",
+    a: "Lima",
+    b: "Cusco",
+    c: "Trujillo",
+    d: "Piura",
+    answer: "a"
+  },
+  {
+    question: "How many countries still have the shilling as currency?",
+    a: "6",
+    b: "7",
+    c: "10",
+    d: "4",
+    answer: "d"
+  }
+];
